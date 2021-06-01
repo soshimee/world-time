0cc9ff1221f93e3d174f5bfad28a2571e60f4d90
BuildTools <unconfigured@null.spigotmc.org>
2021-06-01 23:25:25 +0900

    Bugfix+design

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ const time = document.querySelector(".time");
 const city = document.querySelector(".city");
 const table = document.querySelector("table");
 
-dateTime.value = new Date().toISOString().split('.')[0];
-
 Date.prototype.addHours = function(h) {
 	this.setTime(this.getTime() + (h*60*60*1000));
 	return this;
 }
+
+dateTime.value = new Date().addHours(9).toISOString().split('.')[0];
 
 function update() {
 	const newTime = new Date(dateTime.value);
@@ -21,8 +21,10 @@ function update() {
 			timeDiff = 3;
 			break;
 		case "us_la":
-		case "cr_sj":
 			timeDiff = -16;
+			break;
+		case "cr_sj":
+			timeDiff = -15;
 			break;
 		case "us_ws":
 			timeDiff = -13;
@@ -39,4 +41,18 @@ function update() {
 	title.style.top = `${(window.innerWidth / 5) - (title.getBoundingClientRect().height / 2)}px`;
 	requestAnimationFrame(update);
 }
-requestAnimationFrame(update);
+
+table.style.opacity = "0";
+title.style.top = "-500px";
+title.style.transition = "top 1s";
+setTimeout(() => {
+	table.style.transition = "opacity 1s";
+}, 0);
+title.style.left = `${(window.innerWidth / 2) - (title.getBoundingClientRect().width / 2)}px`;
+title.style.top = `${(window.innerWidth / 5) - (title.getBoundingClientRect().height / 2)}px`;
+table.style.left = `${(window.innerWidth / 2) - (table.getBoundingClientRect().width / 2)}px`;
+table.style.top = `${(window.innerHeight / 2) - (table.getBoundingClientRect().height / 2)}px`;
+setTimeout(() => {
+	table.style.opacity = "1";
+	requestAnimationFrame(update);
+}, 1000);
